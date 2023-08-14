332db8fd3d539cc3507452e83cd493429322ef88
zhisiqi@163.com <zhisiqi@163.com>
2023-08-14 07:52:29 +0000

    test

--- 111/test01.py
+++ 111/test01.py
@@ -2,12 +2,13 @@
 # MAGIC CREATE WIDGET TEXT ADFParameter DEFAULT ""
 
 from pyspark.sql import SparkSession
-
+#from pyspark.context import SparkContex
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL basic example") \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
+#sc = SparkContext.getOrCreate()
 def ctbl():
     spark.sql("CREATE TABLE ct.f"
           "("
--- 111/test03.py
+++ 111/test03.py
@@ -1,3 +1,8 @@
 # Databricks notebook source
 import sys
 print("\n".join(sys.path))
+
+# COMMAND ----------
+
+import test01
+test01.ctbl()
