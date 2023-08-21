e6787e840d842107d3630558a2412bce589bf7e2
zhisiqi@163.com <zhisiqi@163.com>
2023-08-21 02:50:19 +0000

    test

--- 111/Sleep.py
+++ 111/Sleep.py
@@ -1,2 +1,6 @@
-import time
-time.sleep(7200)
+import sys
+import os
+
+# In the command below, replace <username> with your Databricks user name.
+sys.path.append(os.path.abspath('/Workspace/Repos/Test/ADF/111'))
+
--- 222/test02.py
+++ 222/test02.py
@@ -1,4 +1,11 @@
 # MAGIC %sql
 # MAGIC SELECT * FROM shouce01
+
+import sys
+import os
+
+# In the command below, replace <username> with your Databricks user name.
+sys.path.append(os.path.abspath('/Workspace/Repos/Test/ADF/111'))
+
 import test01
 test01.ctbl()
