6e037927aa0a4ef9fde18699dee7b73279bffd6a
zhisiqi@163.com <zhisiqi@163.com>
2023-08-21 05:53:56 +0000

    test

--- 222/test02.py
+++ 222/test02.py
@@ -1,11 +1,6 @@
-# MAGIC %sql
-# MAGIC SELECT * FROM shouce01
-
 import sys
 import os
-
-# In the command below, replace <username> with your Databricks user name.
-sys.path.append(os.path.abspath('/Workspace/Repos/Test/ADF/111'))
-
 import test01
+
+sys.path.append(os.path.abspath('/Workspace/Repos/Test/ADF/111'))
 test01.ctbl()
